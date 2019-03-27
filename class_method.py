fea460d983908bdfa283fd84a7b7d77afb41c6df
dernikevgen <dernik1995@mail.ru>
2019-03-27 03:42:19 +0300

    Create func connect to database

---
+++ class_method.py
@@ -0,0 +1,81 @@
+import psycopg2
+from contextlib import closing
+from psycopg2.extras import DictCursor
+
+
+def select_table(column_name, table_name, id):
+    '''
+    Делаем запрос к базе данных
+    '''
+    with closing(psycopg2.connect(dbname='verbs',
+                                  user='postgres',
+                                  password='654123',
+                                  host='localhost')) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            cursor.execute("SELECT {} FROM {} WHERE id={}".format(
+                column_name,
+                table_name,
+                id)
+            )
+            res = cursor.fetchone()
+            return "".join(res)
+
+
+def len_table(table_name):
+    '''
+    Получение длинны таблицы
+    дла корректного перебора элементов.
+    '''
+    with closing(psycopg2.connect(dbname='verbs',
+                                  user='postgres',
+                                  password='654123',
+                                  host='localhost')) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT max(id) FROM {}".format(table_name))
+            res = cursor.fetchone()
+            return int(''.join(map(str, res)))
+
+
+def insert_table(table_name, v1, v2, v3, vt):
+    '''
+    Добавление значений в таблицу
+    '''
+    with closing(psycopg2.connect(dbname='verbs',
+                                  user='postgres',
+                                  password='654123',
+                                  host='localhost')) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("INSERT INTO {} (one_form, two_form, three_form, translate)"
+                           " VALUES ('{}','{}','{}','{}')".format(table_name, v1, v2, v3, vt))
+            conn.commit()
+
+
+def delete_val_table():
+    '''
+    Удаление элементов из таблицы
+    '''
+    with closing(psycopg2.connect(dbname='verbs',
+                                  user='postgres',
+                                  password='654123',
+                                  host='localhost')) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("DELETE FROM verbs_form WHERE id=9")
+            conn.commit()
+
+
+len__tb = len_table('verbs_form')
+for i in range(1, len__tb+1):
+    a = select_table('one_form', 'verbs_form', i)
+    two = select_table('two_form', 'verbs_form', i)
+    three = select_table('three_form', 'verbs_form', i)
+    trans = select_table('translate', 'verbs_form', i)
+    print(a)
+    answer2 = input('Введите вторую форму: ')
+    print('True' if answer2 == two else 'False')
+    print(two)
+    answer3 = input('Введите третью форму: ')
+    print('True' if answer3 == three else 'False')
+    print(three)
+    answer_t = input('Введите перевод: ')
+    print('True' if answer_t == trans else 'False')
+    print(answer_t)
